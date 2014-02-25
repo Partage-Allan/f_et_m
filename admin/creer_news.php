1323242c815855d2245ad4d563cb04eb840022c7
AzaFixe <AzaFixe@Azaelya>
2014-02-25 20:43:01 +0100

    commit JD

--- admin/creer_news.php
+++ admin/creer_news.php
@@ -17,20 +17,26 @@
                     <li class="list"><a href="news.php">Gestion News</a>
                         <ul class="nav-sub">
                             <li><a href="creer_news.php">Creer une news</a></li>
-                </ul>
+                        </ul>
+                    </li>
                 </ul>
             
-            <h1>Creer une nouvelle news !</h1><br/><br/>     
-                        <form method="post" action="creer_news.php"/>
-                            Titre : <input type="text" name="titre_new" style="margin-left: 20px;"/><br/><br/>
-                            Contenu: <textarea name=contenu_new style="width:300px;height:150px;"></textarea><br/><br/>    
-                            <input type="submit" value="Creer news" style="margin-left: 160px"/>
-                        </form><br/><br/>
-            <?php
-                require ("sql.inc.php");
-                $sql="INSERT INTO news (titre,contenu) VALUES ('titre_new:'contenu_new')";
-                $resultat = executer_requete($sql);
-                header("Location: news.php");
-            ?>
-                        
+                <h1>Creer une nouvelle news !</h1><br/><br/>     
+                <form method="post" action="creer_news.php"/>
+                    Titre : <input type="text" name="titre_new" id="titre_new" style="margin-left: 20px;"/><br/><br/>
+                    Contenu: <textarea name=contenu_new id="contenu_new" style="width:300px;height:150px;"></textarea><br/><br/>    
+                    <input type="submit" value="Creer news" style="margin-left: 160px"/>
+                </form><br/><br/>                     
             </div>
+            
+            <?php
+                    require ("sql.inc.php");
+                    if(!empty($_POST))
+                    {
+                        extract($_POST);
+                        $date = date('Y-m-d H:i:s');
+                        $sql= "INSERT INTO news VALUES ('', '$titre_new', '$contenu_new', '$date')";
+                        $resultat = executer_requete($sql);
+                        header("Location: news.php");
+                    }
+            ?>
